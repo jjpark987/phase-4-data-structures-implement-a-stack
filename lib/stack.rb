1a59eeb80611f98414ff9368e41693ececa885d9
Jason Park <jjpark987@gmail.com>
2023-07-27 12:04:34 -0700

    commit tree 9ed8b0b9a03a954fb2885315c4df3b01285cf02e

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,40 @@
-# your code here
+class Stack
+    def initialize limit = 5
+        @stack = []
+        @limit = limit
+    end
+
+    def push value
+        if @limit && @stack.size < @limit
+            return @stack << value
+        end
+        raise StandardError, 'Stack Overflow'
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.size
+    end
+
+    def empty?
+        @stack.size == 0
+    end
+
+    def full?
+        @stack.size == @limit
+    end
+
+    def search value
+        if @stack.include?(value)
+            return @stack.size - @stack.index(value) - 1
+        end
+        -1
+    end
+end
